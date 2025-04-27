911225c57dece9dd8388b67d77c1946cd5967a87
Deivid Micael <deividmickael@gmail.com>
2025-04-27 07:13:33 -0300

    feat: initialize backend with NestJS framework

---
+++ backend/src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
+
+  console.log(
+    `Server is running on http://localhost:${process.env.PORT ?? 3000}`,
+  );
+}
+
+bootstrap().catch((error) => {
+  console.error('Error starting the server:', error);
+  process.exit(1);
+});
